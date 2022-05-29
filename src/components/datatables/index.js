d9f9e5db4e290147c5a380f476cfb223bb6c2f83
KanaadDeshpande <26kanaad@gmail.com>
2022-05-29 10:11:08 +0530

    Wheeeeee again.

---
+++ src/components/datatables/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./styles/datatables";
+
+const DataTables = () => {
+  return <Container>Hello</Container>;
+};
+
+export default DataTables;
---
+++ src/pages/admin/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { DataTables } from "../../components";
+
+const Admin = () => {
+  return (
+    <>
+      <DataTables />
+    </>
+  );
+};
+
+export default Admin;
