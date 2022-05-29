7735e88780139e44fcd57166a047c7f915c743f4
jayeshkavedia <jayeshkavedia24@gmail.com>
2022-05-29 11:36:18 +0530

    Navbar init

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,45 +5,45 @@ import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlin
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
-import { NavbarContainer } from "./styles";
+import { InputContainer, ItemsContainer, NavbarContainer, SearchContainer, WrapperContainer, ItemContainer } from "./styles";
 
 const Navbar = () => {
   return (
     <NavbarContainer>
-      <div className="wrapper">
-        <div className="search">
-          <input type="text" placeholder="Search..." />
+      <WrapperContainer>
+        <SearchContainer>
+          <InputContainer type="text" placeholder="Search..." />
           <SearchOutlinedIcon />
-        </div>
-        <div className="items">
-          <div className="item">
+        </SearchContainer>
+        <ItemsContainer>
+          <ItemContainer>
             <LanguageOutlinedIcon className="icon" />
             English
-          </div>
+          </ItemContainer>
 
-          <div className="item">
+          <ItemContainer>
             <FullscreenExitOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
+          </ItemContainer>
+          <ItemContainer>
             <NotificationsNoneOutlinedIcon className="icon" />
             <div className="counter">1</div>
-          </div>
-          <div className="item">
+          </ItemContainer>
+          <ItemContainer>
             <ChatBubbleOutlineOutlinedIcon className="icon" />
             <div className="counter">2</div>
-          </div>
-          <div className="item">
+          </ItemContainer>
+          <ItemContainer>
             <ListOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
+          </ItemContainer>
+          <ItemContainer>
             <img
               src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
               alt=""
               className="avatar"
             />
-          </div>
-        </div>
-      </div>
+          </ItemContainer>
+        </ItemsContainer>
+      </WrapperContainer>
     </NavbarContainer>
   );
 };
--- src/components/navbar/styles/index.js
+++ src/components/navbar/styles/index.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components/macro'
+import { LanguageOutlined } from "@mui/icons-material";
+// import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+// import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+// import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+// import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 export const NavbarContainer = styled.div`
   height: 50px;
@@ -7,4 +12,74 @@ export const NavbarContainer = styled.div`
   align-items: center;
   font-size: 14px;
   color: #555;
+`
+export const WrapperContainer = styled.div`
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+export const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 0.5px solid lightgray;
+  padding: 3px;
+`
+
+export const InputContainer = styled.input`
+  border: none;
+  outline: none;
+  background: transparent;
+  &::placeholder {
+    font-size: 12px;
+  }
+`
+export const ItemsContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+  position: relative;
+`
+
+export const LanguageOutlinedIcon = styled(LanguageOutlined)`
+  font-size: 20px;
+`
+// export const FullscreenExitOutlinedIconContainer = styled(FullscreenExitOutlinedIcon)`
+//   font-size: 20px;
+// `
+// export const NotificationsNoneOutlinedIconContainer = styled(NotificationsNoneOutlinedIcon)`
+//   font-size: 20px;
+// `
+// export const ChatBubbleOutlineOutlinedIconContainer = styled(ChatBubbleOutlineOutlinedIcon)`
+//   font-size: 20px;
+// `
+// export const ListOutlinedIconContainer = styled(ListOutlinedIcon)`
+//   font-size: 20px;
+// `
+
+export const AvatarContainer = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+`
+
+export const CounterContainer = styled.img`
+  width: 15px;
+  height: 15px;
+  background-color: red;
+  border-radius: 50%;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 10px;
+  font-weight: bold;
+  position: absolute;
+  top: -5px;
+  right: -5px;
 `
